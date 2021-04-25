414745094717b720002ed0e37ecf2824042318a0
Марина Варнавская <m.varnavskaya@litebox.ru>
2021-04-25 21:41:44 +0400

    task6

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from brain_games import parity
+
+
+def greet():
+    print('Welcome to the Brain Games!')
+    parity.is_parity()
+
+
+def main():
+    greet()
+
+
+if __name__ == '__main__':
+    greet()
