2a1d1c5e5ee774a8732b047a9c007c820525e4e3
Марина Варнавская <m.varnavskaya@litebox.ru>
2021-04-28 20:29:43 +0400

    gcd

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -1,4 +1,4 @@
-from brain_games.games import even, calc, gsd, progress, prime
+from brain_games.games import even, calc, gcd, progress, prime
 
 
-__all__ = ('even', 'calc', 'gsd', 'progress', 'prime')
+__all__ = ('even', 'calc', 'gcd.py', 'progress', 'prime')
