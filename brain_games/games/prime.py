fd2a6e7964d66ecf3ff346bcdffc76d0407b528b
Марина Варнавская <m.varnavskaya@litebox.ru>
2021-04-28 21:09:39 +0400

    prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,13 +7,13 @@ WELCOME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(num):
     if num < 2:
-        return 'no'
+        return 'yes'
     i = 2
     while i <= num / 2:
         if num % i == 0:
-            return 'yes'
+            return 'no'
         i += 1
-    return 'no'
+    return 'yes'
 
 
 def generation():
